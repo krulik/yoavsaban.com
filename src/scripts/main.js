0f35bd71679dc81c655550365407adbdb0d31579
jeniapus <41265117+jeniapus@users.noreply.github.com>
2019-05-26 22:03:12 +0300

    added-little-fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -128,6 +128,7 @@ for (let scrollToLink of scrollToLinks) {
 document.addEventListener("click", function(event) {
 if (event.target.closest(".MainNav-list")) return;
 if(event.target.closest(".MainNav-toggle")) return;
+if(event.target.closest(".MainNav")) return;
 if(navSelector.classList.contains("isOpen")) {
   navSelector.classList.remove('isOpen');
 } else {
